387d0b5ebff90a6509afae7f5bea62b57d87b799
ramzi-omari <ramziomari1995@gmail.com>
2021-05-01 23:54:28 +0100

    Background settings

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,7 +31,7 @@ const Header = () => {
           <span>SERIES</span>
         </a>
       </NavMenu>
-      <UserImg src="  "></UserImg>
+      <UserImg src="https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=749&q=80 "></UserImg>
     </Nav>
   )
 }
